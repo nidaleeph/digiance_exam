7821fbc58ed119b18b3affb36eb0455fdc6819c7
Jonathan Rivera <nidaleeph@gmail.com>
2025-01-16 14:35:10 +0800

    Fix storage

--- resources/js/store/state.js
+++ resources/js/store/state.js
@@ -1,6 +1,5 @@
 export default {
     user: {
-        // Fetch token from localStorage, clear any leftover from sessionStorage
         token: (() => {
             const token = localStorage.getItem("TOKEN");
             sessionStorage.removeItem("TOKEN");
